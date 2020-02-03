7773f60bcac0419dcc48ded9fbac06c3aaf336ae
Tanveer Salim <Tanveer.Salim@protonmail.com>
2020-02-03 00:17:15 -0600

    Now prm can correctly detect directories. To recursively list contents of subdirectories, you must concatenate dirent name to basepath

--- lsa.c
+++ lsa.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <sys/types.h>
@@ -6,29 +7,13 @@
 #include <unistd.h>
 #include <errno.h>
 
-int is_directory(const char*path)	{
-	
-	DIR * directory = opendir(path);
-	
-	if (errno==ENOTDIR)	{
-		return 0;
-	}
-
-	if (directory!=NULL)	{
-		closedir(directory);
-		return 1;
-	}
-
-	return -1;
-
-}
 
 
-void lsa(char*argv)	{
+void lsa(char*basepath)	{
 	
 	struct dirent *de; //Pointer for entry
 
-	DIR * dr = opendir(argv);
+	DIR * dr = opendir(basepath);
 
 	if (dr == NULL) // opendir
 	{
@@ -37,12 +22,15 @@ void lsa(char*argv)	{
 
 	while ((de = readdir(dr)) != NULL)		{
 		
-		printf("%s\n",de->d_name);
-
-		if(is_directory(de->d_name) && !strcmp(de->d_name,".") && !strcmp(de->d_name,".."))	{
+		printf("%s:%d\n",de->d_name,de->d_type==DT_DIR);
+		
+		if ((de->d_type==DT_DIR)&&!(strcmp(de->d_name,".")==0)&&!(strcmp(de->d_name,"..")==0))	{
 			
-			lsa(de->d_name);	
-						
+			printf("Made it\n");
+
+			//You actually need to concatenate the full path name from argv, rename it basepath
+
+			lsa(de->d_name);
 		}
 	}
 
@@ -53,6 +41,7 @@ void lsa(char*argv)	{
 int main(int argc,char**argv)	{
 	
 	lsa(argv[1]);
-
+	
+	
 	return 0;
 }
